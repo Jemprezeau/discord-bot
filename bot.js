0f4eec06fa9456964c85e74e2cbb621cd10cda9c
Jemprezeau <55039765+Jemprezeau@users.noreply.github.com>
2019-09-07 23:32:56 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'bacon') {
+
+       message.reply('hair');
+
+       }
+
+});
+
+ 
+
+//
+
+client.login(process.env.BOT_TOKEN);//
